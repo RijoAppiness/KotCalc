1584cbfb7905db62910e3b8be5b5a57cc27bb789
sreeramappiness <sreeram@appinessworld.com>
2019-11-18 17:10:57 +0530

    commit from sreeram

--- app/src/main/java/com/example/kotcalc/MainActivity.kt
+++ app/src/main/java/com/example/kotcalc/MainActivity.kt
@@ -7,7 +7,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -275,8 +274,4 @@ fun multiOperation(){
     println(calcEngine.getDisplayString())
     calcEngine.pushKey(KEY.EQUAL())
     println(calcEngine.getDisplayString())
-
-
-
-
 }
